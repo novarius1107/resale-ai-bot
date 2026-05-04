30d45f4e928ffe8f1ba9a7a73f62097d74c72ef7
novarius1107 <zxrpg6jms6@privaterelay.appleid.com>
2026-05-04 10:32:17 -0800

    Create session.py

---
+++ db/session.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./resale_bot.db")
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
